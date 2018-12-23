ca988ab2f599023cd3475e459caa621671dac273
YeahNahCunt <42754470+YeahNahCunt@users.noreply.github.com>
2018-12-24 05:19:02 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 import Auth #File contains login and token#
 import configparser
 import discord
+import random
 import ctx
+import json
 from discord.ext import commands
+from discord.ext.commands import Bot
+from imgurpython import ImgurClient
 
 #clients or defining variables#
 
@@ -16,6 +20,11 @@ async def on_ready():
     await client.change_presence(game=discord.Game(name='.help'))
     print('Bot is ready.')
     print(discord.__version__)
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('*******************')
+
 
 @client.command()
 async def ping(ctx):
@@ -41,47 +50,48 @@ async def help(ctx):
     embed.set_thumbnail(url='https://www.nydailynews.com/resizer/gVvubfLWIcXCqhWyJYTiVte36V4=/800x0/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/3UXXG5RXAOTFCM4K4QXN6WL3SM.jpg')
     embed.set_footer(text='Any reccomendations/requests/bugs can be directed to Cunt ( Disturbed#3853 )')
     embed.add_field(name=':spy: General :spy:', value=
-    '`ping      :` Ping bot to see if online\n'+
-    '`pingNSFW  :` Ping bot to see if channel is NSFW', inline=False)
+    '`ping               :` Ping bot to see if online\n'+
+    '`pingNSFW           :` Ping bot to see if channel is NSFW', inline=False)
     embed.add_field(name=':baby_bottle: Safe for work commands :baby_bottle:', value=
-    '`meme      :` For dank Memes\n'+
-    '`gifs      :` For dank Gifs', inline=False)
+    '`iwant memes        :` For dank Memes\n'+
+    '`iwant gifs         :` For dank Gifs', inline=False)
     embed.add_field(name=':underage: General NSFW :underage:', value=
-    '`amateur   :` Amateur girls\n'+
-    '`anal      :` For your anal fixations\n'+
-    '`ass       :` For some sweet booty\n'+
-    '`boobs     :` Mmmmhhh boobs\n'+
-    '`gonewild  :` random image from gonewild subreddit\n'
-    '`milf      :` Hot mommas\n'+
-    '`NSFWgifs  :` Sexy gifs\n'+
-    '`petite    :` Petite cute girls\n'+
-    '`teen      :` Legal teens', inline=False)
+    '`nsfw amateur       :` Amateur girls\n'+
+    '`nsfw anal          :` For your anal fixations\n'+
+    '`nsfw ass           :` For some sweet booty\n'+
+    '`nsfw boobs         :` Mmmmhhh boobs\n'+
+    '`nsfw gonewild      :` random image from gonewild subreddit\n'
+    '`nsfw milf          :` Hot mommas\n'+
+    '`nsfw gifs          :` Sexy gifs\n'+
+    '`nsfw petite        :` Petite cute girls\n'+
+    '`nsfw teen          :` Legal teens', inline=False)
     embed.add_field(name=':fire: KINK NSFW :fire:', value=
-    '`blowjob   :` Sexy BJs\n'+
-    '`BDSM      :` Hot, sweaty and rough\n'+
-    '`celeb     :` Celebs and their best nude/sexy content\n'+
-    '`cum       :` We got your protein fix here\n'+
-    '`fit       :` Female Body Perfection\n'
-    '`gay       :` A place for sexy guys\n'+
-    '`lesbian   :` Girl on girl action\n'+
-    '`orgasm    :` "O" Faces. Faces of Ecstasy\n'+
-    '`outfits   :` Hot pics of people in NSFW Outfits\n'+
-    '`pussy     :` It is not about cats\n'+
-    '`thicc     :` Sexy curves\n'+
-    '`thigh     :` Thick thighs and luscious curves\n'+
-    '`trans     :` Traps and trans\n'+
-    '`hentai    :` Rule 34 an dother weeb trash', inline=False)
+    '`nsfw blowjob       :` Sexy BJs\n'+
+    '`nsfw BDSM          :` Hot, sweaty and rough\n'+
+    '`nsfw celeb         :` Celebs and their best nude/sexy content\n'+
+    '`nsfw cum           :` We got your protein fix here\n'+
+    '`nsfw fit           :` Female Body Perfection\n'
+    '`nsfw gay           :` A place for sexy guys\n'+
+    '`nsfw lesbian       :` Girl on girl action\n'+
+    '`nsfw orgasm        :` "O" Faces. Faces of Ecstasy\n'+
+    '`nsfw outfits       :` Hot pics of people in NSFW Outfits\n'+
+    '`nsfw pussy         :` It is not about cats\n'+
+    '`nsfw thicc         :` Sexy curves\n'+
+    '`nsfw thigh         :` Thick thighs and luscious curves\n'+
+    '`nsfw trans         :` Traps and trans\n'+
+    '`nsfw hentai        :` Rule 34 an dother weeb trash', inline=False)
     embed.add_field(name=':earth_africa: Ethnic NSFW :earth_asia:', value=
-    '`ethnic    :` For your random exotic cravings\n'+
-    '`arab      :` Middle eastern hotties\n'+
-    '`asian     :` For that Eastern taste\n'+
-    '`black     :` Tastes like chocolate\n'+
-    '`latino    :` Hot blooded latin girls\n'
-    '`indian    :` Spicy indian girls\n'+
-    '`white     :` Pale cuties'
+    '`nsfw ethnic        :` For your random exotic cravings\n'+
+    '`nsfw arab          :` Middle eastern hotties\n'+
+    '`nsfw asian         :` For that Eastern taste\n'+
+    '`nsfw black         :` Tastes like chocolate\n'+
+    '`nsfw latino        :` Hot blooded latin girls\n'
+    '`nsfw indian        :` Spicy indian girls\n'+
+    '`nsfw white         :` Pale cuties'
     , inline=False)
 
     await ctx.send(embed=embed)
+
 
 ##Load Cogs##
 client.load_extension('cogs.SFWCommands')
