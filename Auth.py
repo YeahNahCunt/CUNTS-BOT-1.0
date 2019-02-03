670528ea9e02e1d5944ae50e7b15c6adc388a743
YeahNahCunt <42754470+YeahNahCunt@users.noreply.github.com>
2019-02-03 15:45:31 +0800

    Delete Auth.py

--- Auth.py
+++
@@ -1,8 +0,0 @@
-client_id="Imgur Client ID here"
-client_secret="Imgur secret code here"
-imgur_username="imgur login username"
-imgur_password="imgur login password"
-
-discord_token="Discord token here"
-
-##file just is for private details update as you need##
